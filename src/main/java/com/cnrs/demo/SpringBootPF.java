7b5570687da66143caa46001226f6f62be21c38d
GABSI <firas.gabsi@gmail.com>
2024-05-13 09:28:26 +0200

    Ajout de la squelette du projet

---
+++ src/main/java/com/cnrs/demo/SpringBootPF.java
@@ -0,0 +1,14 @@
+package com.cnrs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootPF {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPF.class, args);
+    }
+
+}
